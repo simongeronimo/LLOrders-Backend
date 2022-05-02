68523cb365dfc84958e3318932f254504581fcdb
Simon Mesber <simon.mesber@correo.unimet.edu.ve>
2022-05-02 12:30:15 -0400

    Env Variables

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,8 @@
 import express from 'express';
+import { testEnvironmentVariable } from '../config';
 
 const indexRouter = express.Router();
 
-indexRouter.get('/', (req, res) =>
-  res.status(200).json({ message: 'Welcome to Express API template' })
-);
+indexRouter.get('/', (req, res) => res.status(200).json({ message: testEnvironmentVariable }));
 
 export default indexRouter;
